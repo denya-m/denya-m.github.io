4058df4856934d57e6bda879e4e38c60f58d04b5
denya-m <43039696+denya-m@users.noreply.github.com>
2019-02-14 17:24:08 +0300

    Valentine-s-Day  Completed

--- Valentine-s-Day/script.js
+++ Valentine-s-Day/script.js
@@ -4,3 +4,12 @@ $("#container").click(function() {
 }).mouseleave(function() {
   audio.pause();
 });
+
+if($(window).width() > 740){
+  var audio = $("#sound")[0];
+$("#container").mouseenter(function() {
+  audio.play();
+}).mouseleave(function() {
+  audio.pause();
+});
+}
